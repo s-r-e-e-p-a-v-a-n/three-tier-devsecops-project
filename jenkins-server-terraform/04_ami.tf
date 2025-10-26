2eae98778482cdfec7bf145c7d70fabef8f5f97e
deepthisusmithapitani <deepthisusmitha.deepu@gmail.com>
2025-10-26 18:24:06 +0530

    Amazon linux

--- jenkins-server-terraform/04_ami.tf
+++ jenkins-server-terraform/04_ami.tf
@@ -1,12 +1,11 @@
-# AWS AMI data source to retrieve the most recent Ubuntu 22.04 AMI image
-data "aws_ami" "ami" {
+# Use Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux_2023" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["amzn-2023-*-x86_64*"]
   }
 
-  # Owner of the Ubuntu AMI (Canonical)
-  owners = ["099720109477"]
+  owners = ["137112412989"] # Amazon official AMI owner ID
 }
